97b3b54647ef1fd27e66fd9a3d56e1f9a35e8d05
Amritanshu Dash <amritanshudash6677@gmail.com>
2024-05-31 16:04:48 +0530

    start of the project

---
+++ BarCode Scanner/BarCode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCode_ScannerApp.swift
+//  BarCode Scanner
+//
+//  Created by Amritanshu Dash on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
